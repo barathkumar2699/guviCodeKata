fbbe8741788524a6399214bfaa38d8ff125c82ee
barathkumar2699 <51689598+barathkumar2699@users.noreply.github.com>
2019-06-11 12:26:57 +0530

    Update EncodedStringADD.c

--- EncodedStringADD.c
+++ EncodedStringADD.c
@@ -5,10 +5,12 @@ int main() {
 	char a[1000],b[1000];
 	scanf("%s",a);
 	scanf("%s",b);
+	int n1=strlen(a);
+	//int n2=strlen(b);
 	for(int i=0;a[i];i++)
 	{
-	  //  printf("%c",(((a[i]-96 )+ (b[i]-96))%26+97)-1);
-	    printf("%c",((a[i]+b[i]-96))%123+97);
+	    printf("%c",(((a[i]-97 )+ (b[i]-97))%26+97)+1);
+	   // printf("%c",((a[i]+b[i]-96))%122);
 	}
 	return 0;
 }
