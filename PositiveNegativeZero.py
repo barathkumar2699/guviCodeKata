8a9bb988585b8fa258b6a85ec764994f863fe106
barathkumar2699 <51689598+barathkumar2699@users.noreply.github.com>
2019-06-11 10:11:21 +0530

    Create PositiveNegativeZero.py

---
+++ PositiveNegativeZero.py
@@ -0,0 +1,11 @@
+try:
+    n=int(input())
+    if(n>0):
+        print('Positive')
+    elif(n<0):
+        print('Negative')
+    elif(n==0):
+        print('Zero')
+    
+except:
+     print('Invalid')
